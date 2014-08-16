865b053ad019bd8d395c7e80e96bdb3d916f50ea
Tim Mitra <twitter@it-guy.com>
2014-08-16 02:07:59 -0400

    excluding testimonial posts on blog page

--- wp-content/themes/lander/functions.php
+++ wp-content/themes/lander/functions.php
@@ -16,4 +16,12 @@ add_action( 'wp_enqueue_scripts', 'lander_scripts');
 
 add_image_size(  'testimonial-mug', 253, 253, true);
 
+function exclude_testimonials( $query ) {
+    if ( !$query->is_category( 'testimonials' ) && $query->is_main_query() ) {
+        $query->set( 'cat', '-66' );
+    }
+}
+
+add_action( 'pre_get_posts', 'exclude_testimonials' );
+
 ?>
