07b7f987332b156ab1b9b41390c5fe6f336c76d3
Tim Mitra <tim@it-guy.com>
2025-09-18 16:53:44 -0400

    enqueue the theme

--- wp-content/themes/my-block-theme/functions.php
+++ wp-content/themes/my-block-theme/functions.php
@@ -1,4 +1,17 @@
 <?php
 // disable all the default block patterns
 remove_theme_support( 'core-block-patterns' );
+
+
+function my_block_theme_enqueue_styles() {
+    wp_enqueue_style(
+        'my-block-theme-style',
+        get_stylesheet_uri(),
+        array(),
+        wp_get_theme()->get( 'Version')
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'my_block_theme_enqueue_styles' );
+
 ?>
